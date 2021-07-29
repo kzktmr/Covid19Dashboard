2c5c7e0055e1d6b7e9404f65e2dafc941616ca82
Kazuki Tamura <kazukitamura@MacBookAir2019.local>
2021-07-29 23:31:31 +0900

    remove weekday

--- get-data.R
+++ get-data.R
@@ -8,12 +8,10 @@ ckanr_setup(url = "https://ckan.open-governmentdata.org/")
 # 福岡県　新型コロナウイルス感染症　陽性者発表情報
 res <- resource_show("0430a12e-568c-4a6a-bed8-51621f47c6e5")
 
-patients <- 
+tmp <- 
   read_csv(res$url, 
            skip = 1, col_types = "___D_cccl__",
            col_names = c("date", "address", "age", "sex", "untraceable")) %>% 
-  filter(!is.na(date)) %>% 
-  mutate(weekday = weekdays(date, abbreviate = TRUE)) %>% 
-  mutate(holiday = if_else(weekday %in% c("土", "日") | zipangu::is_jholiday(date), TRUE, FALSE)) #%>% 
+  filter(!is.na(date))
 
-write_csv(patients, "data/patients.csv")
+write_csv(tmp, "data/patients.csv")
