f68aff92b46f52592640d86a5cffe5fce2f51d6b
Kazuki Tamura <kazukitamura@MacBookAir2019.local>
2021-08-29 08:10:46 +0900

    change roll_mean align for graph of deaths

--- get-data.R
+++ get-data.R
@@ -84,7 +84,8 @@ tmp <- tibble(date = c(as.Date("2021-01-01"), Sys.Date())) %>%
                          date < "2021-08-18" ~ 1433,
                          date < "2021-08-20" ~ 1444,
                          date < "2021-08-24" ~ 1455,
-                         TRUE ~ 1460))
+                         date < "2021-08-27" ~ 1460,
+                         TRUE ~ 1472))
 write_csv(tmp, "data/sickbeds.csv")
 
 # ワクチン接種状況
